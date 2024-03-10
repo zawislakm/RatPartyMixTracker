bcd67f49dbf0f6e87403f2e8173b9839d18c8346
zawislakm <maksymilian.zawislak@gmail.com>
2024-03-10 12:05:07 +0100

    API KEYS change & cat .env rm

--- src/API/__init__.py
+++ src/API/__init__.py
@@ -1,10 +1,9 @@
-import json
 import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
-API_KEYS = json.loads(os.getenv("RAT_PARTY_MIX_API_KEYS"))
+API_KEYS = os.getenv("RAT_PARTY_MIX_API_KEYS")
 SECRET_KEY = os.getenv("RAT_PARTY_MIX_SECRET_KEY")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS"))
